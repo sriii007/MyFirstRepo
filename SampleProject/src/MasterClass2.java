71fd27f96a896e18dba5a3c9dfab060f3496b329
sriii007 <95336705+sriii007@users.noreply.github.com>
2021-12-01 18:12:16 +0530

    Update MasterClass2.java

--- SampleProject/src/MasterClass2.java
+++ SampleProject/src/MasterClass2.java
@@ -2,6 +2,7 @@
 public class MasterClass2 {
 	public static void main(String[] args) {
 		System.out.println("MasterClass is added 2nd time");
+		System.out.println("Made changes through master branch");
 	}
 
 }
