2f75508899b7b4e962af0c8e678f678ad6665a63
sriii007 <srinidhi.ss26@gmail.com>
2021-12-01 17:36:02 +0530

    Branch1 created

---
+++ SampleProject/src/BranchClass.java
@@ -0,0 +1,10 @@
+
+public class BranchClass {
+    static int branchClass(int a ,int b){
+	System.out.println("My name is Srinidhi");
+	return a*b;
+}
+public static void main(String[] args) {
+	BranchClass.branchClass(1,2);
+}
+}
